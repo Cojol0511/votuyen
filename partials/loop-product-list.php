d4ef486a23cdaffe30b00280c16c83533d0f6d5e
Cojol0511 <phunghoangphuloc@gmail.com>
2021-10-12 14:39:25 +0700

    edit page list product, add page remote detail

--- partials/loop-product-list.php
+++ partials/loop-product-list.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
 <main id="page-product-list">
-  <?php get_template_part( "template-parts/banner" ,"product"); ?>
+  <section class="banner-product">
+    <div class="banner-text">
+      <h1 class="text-title">製品情報</h1>
+      <h2 class="text-sub-title">高品質＋低コスト＋迅速での開発</h2>
+    </div>
+  </section>
 
-  <section class="product-list">
+  <section class="main">
     <div class="container">
       <div class="row">
         <h1 class="text-title">用途／対応製品：</h1>
---
+++ partials/loop-remote-control.php
@@ -0,0 +1,454 @@
+<?php get_header(); ?>
+<main id="page-remote-control">
+  <section class="banner-product">
+    <div class="banner-text">
+      <h1 class="text-title">製品情報</h1>
+      <h2 class="text-sub-title">お客様のニーズに応え</h2>
+      <h2 class="text-sub-title">無線リモコン</h2>
+    </div>
+  </section>
+
+  <!-- Part 1 -->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">1. 送信機シリーズ:</h1>
+          <div class="col-3">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">EcoTx315A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx426A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx429A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx920A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx2.4G1</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-9 text-end">
+            <img class="product-img"
+              src="<?php bloginfo('template_url'); ?>/assets/images/page-product/remote-control-part1.jpg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴</h2>
+        <ul class="feature-list">
+          <li><span>1.デュアルコアCPU</span>
+          </li>
+          <li><span>2.最適なRF機能</span>
+          </li>
+          <li><span>3.高セキュリティハードウェア／ソフトウェア</span>
+          </li>
+          <li><span>4.RFコアの超低消費電力</span>
+          </li>
+          <li><span>5.IP67防水ケースまたは非防水ケースのたくさんケースシリーズから選択可</span>
+          </li>
+          <li><span>6.バッテリータイプの多くの選択肢（CR2032、AAAx2、AAAx3、AAAx4、AAx2、AAx3、AAx4、AAx6</span>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+    <div class="table">
+      <div class="container">
+        <h2 class="text-sub-title">仕様</h2>
+        <table>
+          <thead>
+            <tr>
+              <td>項目</td>
+              <td>EcoTx315xx</td>
+              <td>EcoTx426A1</td>
+              <td>EcoTx429A1</td>
+              <td>EcoTx920A1</td>
+              <td>EcoTx2.4G1</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>規格</td>
+              <td>ARIB STD-T93</td>
+              <td>ARIB STD-T67</td>
+              <td>ARIB STD-T67</td>
+              <td>ARIB STD-T108</td>
+              <td>ARIB STD-T66</td>
+            </tr>
+            <tr>
+              <td>周波数</td>
+              <td>312.1MHz～315.0MHz</td>
+              <td>426.0250MHz～426.1375MHz</td>
+              <td>429.2500～429.7375MHz</td>
+              <td>920.6MHz～928.0MHz</td>
+              <td>2400MHz～2483.5MHz</td>
+            </tr>
+            <tr>
+              <td>チャネル数</td>
+              <td>設定可</td>
+              <td>間隔12.5kHz: 10CH<br>間隔 6.25kHz: 18CH</td>
+              <td>間隔 12.5kHz: 40CH<br>間隔 6.25kHz: 79CH</td>
+              <td>間隔 200kHz: 38CH</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>変調方式</td>
+              <td>GFSK</td>
+              <td>GFSK</td>
+              <td>GFSK</td>
+              <td>GFSK</td>
+              <td>スペクトラム拡散</td>
+            </tr>
+            <tr>
+              <td>時間制限</td>
+              <td>90s on, 一瞬 off</td>
+              <td>5s on, 2s off </td>
+              <td>連続可</td>
+              <td>4s on, 0.05s off </td>
+              <td>連続可</td>
+            </tr>
+            <tr>
+              <td>変調スピード</td>
+              <td>設定可</td>
+              <td>2.4kbps/4.8kbps</td>
+              <td>2.4kbps/4.8kbps</td>
+              <td>2.4kbps～100kbps</td>
+              <td>250kbps</td>
+            </tr>
+            <tr>
+              <td>送信パワー</td>
+              <td>0.25mW (EIRP)</td>
+              <td>10mW</td>
+              <td>10mW</td>
+              <td>20mW</td>
+              <td>200mW</td>
+            </tr>
+            <tr>
+              <td>温度補償共振器</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+            </tr>
+            <tr>
+              <td>アンテナ</td>
+              <td>内蔵</td>
+              <td>内蔵</td>
+              <td>内蔵</td>
+              <td>内蔵</td>
+              <td>内蔵</td>
+            </tr>
+            <tr>
+              <td>電池</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+            </tr>
+            <tr>
+              <td>電池寿命</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+              <td>ケースにより</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+
+  <!-- Part 2-->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">2. 受信機シリーズ:</h1>
+          <div class="col-3">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">EcoTx315A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx426A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx429A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx920A1</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">EcoTx2.4G1</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-9 text-end">
+            <img class="product-img"
+              src="<?php bloginfo('template_url'); ?>/assets/images/page-product/remote-control-part2.jpg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴</h2>
+        <ul class="feature-list">
+          <li><span>1.デュアルコアCPU</span></li>
+          <li><span>2.最適なRF</span></li>
+          <li><span>3.安全性の高いハードウェア</span></li>
+          <li><span>4.RFコアの超低消費電力</span></li>
+          <li><span>5.IP67防水ケースまたは非防水ケースのたくさんケースシリーズから選択可</span></li>
+          <li><span>6.電源タイプ（DC 5V、DC 12V、DC 24V、AC100-200V、ソーラーパネル）のカスタマイズ可</span></li>
+          <li><span>7.出力タイプ（リレー、オープンコレクター、UART、RS232、SPI、USB ...）のカスタマイズ可</span></li>
+          <li><span>8.安全入力信号、制御信号のカスタマイズ</span></li>
+        </ul>
+      </div>
+    </div>
+    <div class="table">
+      <div class="container">
+        <h2 class="text-sub-title">仕様</h2>
+        <table>
+          <thead>
+            <tr>
+              <td>項目</td>
+              <td>EcoTx315xx</td>
+              <td>EcoTx426A1</td>
+              <td>EcoTx429A1</td>
+              <td>EcoTx920A1</td>
+              <td>EcoTx2.4G1</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>周波数</td>
+              <td>312.1MHz～315.0MHz</td>
+              <td>426.0250MHz～426.1375MHz</td>
+              <td>429.2500～429.7375MHz</td>
+              <td>920.6MHz～928.0MHz</td>
+              <td>2400MHz～2483.5MHz</td>
+            </tr>
+            <tr>
+              <td>受信能力</td>
+              <td>拡張距離IC内蔵</td>
+              <td>拡張距離IC内蔵</td>
+              <td>拡張距離IC内蔵</td>
+              <td>拡張距離IC内蔵</td>
+              <td>拡張距離IC内蔵</td>
+            </tr>
+            <tr>
+              <td>見通し通信距離</td>
+              <td>約300m</td>
+              <td>約1000m</td>
+              <td>約1000m</td>
+              <td>約2000m</td>
+              <td>約10000m</td>
+            </tr>
+            <tr>
+              <td>温度補償共振器</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+              <td>TCXO</td>
+            </tr>
+            <tr>
+              <td>出力タイプ</td>
+              <td>指定可</td>
+              <td>指定可</td>
+              <td>連続可</td>
+              <td>指定可</td>
+              <td>指定可</td>
+            </tr>
+            <tr>
+              <td>アンテナ</td>
+              <td>内蔵／外部</td>
+              <td>内蔵／外部</td>
+              <td>内蔵／外部</td>
+              <td>内蔵／外部</td>
+              <td>内蔵／外部</td>
+            </tr>
+            <tr>
+              <td>電源</td>
+              <td>指定可</td>
+              <td>指定可</td>
+              <td>指定可</td>
+              <td>指定可</td>
+              <td>指定可</td>
+            </tr>
+            <tr>
+              <td>消費電流</td>
+              <td>スリープ 0.1mA<br>受信時 9mA</td>
+              <td>スリープ 0.1mA<br>受信時 9mA</td>
+              <td>スリープ 0.1mA<br>受信時 9mA</td>
+              <td>スリープ 0.1mA<br>受信時 9mA</td>
+              <td>スリープ 0.1mA<br>受信時 9mA</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+
+  <!-- Part 3-->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">3. 赤外線リモコン</h1>
+          <div class="col-4">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">送信機型番: InTx01</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">受信機型番: InRx01</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-8 text-end">
+            <img class="product-img"
+              src="<?php bloginfo('template_url'); ?>/assets/images/page-product/remote-control-part3.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴</h2>
+        <ul class="feature-list">
+          <li><span>1.より良い耐ノイズ性のための二重変調</span></li>
+          <li><span>2.高出力赤外線LED</span></li>
+          <li><span>3.高感度RxLED</span></li>
+          <li><span>4.たくさんケースシリーズから選択可</span></li>
+          <li><span>5.バッテリータイプと電源タイプはたくさんケースシリーズから選択可</span></li>
+          <li><span>6.出力タイプ（リレー、オープンコレクター、UART、RS232、SPI、USB ...）のカスタマイズ可</span></li>
+          <li><span>7.通信距離：屋内約15m、屋外約3m</span></li>
+      </div>
+    </div>
+  </section>
+
+  <!-- Part 4-->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">4. ハイブリッドリモコンシステム</h1>
+          <div class="col-8">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">ハイブリッドリモコン型番: HiTx01</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">ハイブリッドシステム型番: HiSys01</h2>
+                </li>
+              </ul>
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <td>機能</td>
+                  <td>電波</td>
+                  <td>赤外線</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>ハイブリッドリモコン</td>
+                  <td>o</td>
+                  <td>o</td>
+                </tr>
+                <tr>
+                  <td>ハイブリッド送受信機</td>
+                  <td>o</td>
+                  <td>o</td>
+                </tr>
+                <tr>
+                  <td>制御センター</td>
+                  <td>o</td>
+                  <td>x</td>
+                </tr>
+                <tr>
+                  <td>長距離</td>
+                  <td>o</td>
+                  <td>x</td>
+                </tr>
+                <tr>
+                  <td>指向性</td>
+                  <td>x</td>
+                  <td>o</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-4 text-end">
+            <img class="product-img"
+              src="<?php bloginfo('template_url'); ?>/assets/images/page-product/remote-control-part4.jpg" alt="">
+          </div>
+          <div class="col-12">
+            <div class="img-bottom">
+              <img class="product-img"
+                src="<?php bloginfo('template_url'); ?>/assets/images/page-product/remote-control-part4-bottom.png"
+                alt="">
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴:</h2>
+        <ul class="feature-list">
+          <li><span>1.電波と赤外線のハイブリッド方式のリモコンシステム</span></li>
+          <li><span>2.指向性／無指向性／通信距離の用途からモード選定可</span></li>
+          <li><span>3.ハイブリッドモードでより高いセキュリティ性</span></li>
+          <li><span>4.たくさんケースシリーズから選択可</span></li>
+          <li><span>5.バッテリータイプと電源タイプはたくさんケースシリーズから選択可</span></li>
+          <li><span>6.出力タイプ（リレー、オープンコレクター、UART、RS232、SPI、USB ...）のカスタマイズ可</span></li>
+          <li><span>7.通信距離：赤外線の屋内用15m、屋外用3m、電波の屋内外約300m　</span></li>
+        </ul>
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">用途／対応製品</h2>
+        <ul class="feature-list">
+          <li><span>1. 特定小電力送信機</span></li>
+          <li><span>2. 特定小電力受信機</span></li>
+          <li><span>3.有線から無線に変換</span></li>
+          <li><span>4. 産業リモコン</span></li>
+          <li><span>5 . 無線リモコン</span></li>
+          <li><span>6. ISMバンド／315MHz／426MHz／　429MHz／920MHz／2.4GHz</span></li>
+          <li><span>7. カスタマイズ対応</span></li>
+          <li><span>8 . 特殊用途対応</span></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+
+  <?php get_template_part( "template-parts/footer_top" ,"product"); ?>
+
+</main>
+<?php get_footer(); ?>
