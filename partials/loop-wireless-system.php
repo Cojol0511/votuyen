f55833bd82a0e2a8d59c62952967af332158fbed
Cojol0511 <phunghoangphuloc@gmail.com>
2021-10-12 17:15:00 +0700

    finish page wireless-system

---
+++ partials/loop-wireless-system.php
@@ -0,0 +1,167 @@
+<?php get_header(); ?>
+<main id="page-remote-control" class="page-detail-product">
+  <section class="banner-product">
+    <div class="banner-text">
+      <h1 class="text-title">製品情報</h1>
+      <h2 class="text-sub-title">お客様のニーズに応え</h2>
+      <h2 class="text-sub-title">無線リモコン</h2>
+    </div>
+  </section>
+
+  <!-- Part 1 -->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">1. 緊急通報システム</h1>
+          <div class="col-3">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">子機型番: EmCS01</h2>
+                </li>
+                <li>
+                  <h2 class="text-menu">親機型番: EmPS01</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-9 text-end">
+            <img class="product-img"
+              src="<?php bloginfo('template_url'); ?>/assets/images/page-product/wireless-system-part1.jpg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴</h2>
+        <ul class="feature-list">
+          <li><span>1. ワンタッチで呼び出す</span></li>
+          <li><span>2. 双方向通信</span></li>
+          <li><span>3. おおよその場所を取得可能</span></li>
+          <li><span>4. 都内での長距離通信</span></li>
+        </ul>
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">用途／対応製品</h2>
+        <ul class="feature-list">
+          <li><span>1. 緊急通報</span></li>
+          <li><span>2. 呼び出し</span></li>
+          <li><span>3. 介護用</span></li>
+          <li><span>4. 老人ホーム</span></li>
+          <li><span>5. 認知症病者</span></li>
+          <li><span>6. 有線から無線に変換</span></li>
+          <li><span>7. カスタマイズ対応</span></li>
+          <li><span>8. 特殊用途対応</span></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <!-- Part 2 -->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">2. 遠隔監視システム</h1>
+          <div class="col-3">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">型番: ReMS01</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴</h2>
+        <ul class="feature-list">
+          <li><span>1.　マイクロ波センサー（ドップラー周波数）による動きの追跡</span></li>
+          <li><span>2. 環境依存の赤外線式を無くし、高精度の接近または離反の検知可</span></li>
+          <li><span>3.　カメラやレーザなどに比べて霧や雨等の環境変化による影響を受けにくい</span></li>
+          <li><span>4.　クラウド、スマートフォンに接続、制御可</span></li>
+        </ul>
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">用途／対応製品</h2>
+        <ul class="feature-list">
+          <li><span>1. 屋内、屋外の監視</span></li>
+          <li><span>2. 呼吸・拍動などのヘルスケア・モニタリング</span></li>
+          <li><span>3. 浴室内などのプライベート空間での見守り</span></li>
+          <li><span>4. セキュリティ応用</span></li>
+          <li><span>5. 遠隔モニタリング</span></li>
+          <li><span>6. 介護用</span></li>
+          <li><span>7. 老人ホーム</span></li>
+          <li><span>8. 遠隔監視システム</span></li>
+          <li><span>9. 遠隔制御システム</span></li>
+          <li><span>10. 有線から無線に変換</span></li>
+          <li><span>11. センサーネットワーク</span></li>
+          <li><span>12. カスタマイズ対応</span></li>
+          <li><span>13. 特殊用途対応</span></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <!-- Part 3 -->
+  <section class="product">
+    <div class="description">
+      <div class="container">
+        <div class="row">
+          <h1 class="text-title">3. センサーネットワークシステム</h1>
+          <div class="col-3">
+            <div class="menu">
+              <ul>
+                <li>
+                  <h2 class="text-menu">型番: SeNS01</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">特徴</h2>
+        <ul class="feature-list">
+          <li><span>1. 温度、湿度、雨、磁気、光など、さまざまな種類のセンサーの情報を取得</span></li>
+          <li><span>2. クラウド、スマートフォンに接続、制御可</span></li>
+        </ul>
+      </div>
+    </div>
+    <div class="text">
+      <div class="container">
+        <h2 class="text-sub-title">用途／対応製品</h2>
+        <ul class="feature-list">
+          <li><span>1. IoTシステム</span></li>
+          <li><span>2. スマートナーシングホーム</span></li>
+          <li><span>3. スマートホーム</span></li>
+          <li><span>4. スマート農業</span></li>
+          <li><span>5. スマートファクトリー</span></li>
+          <li><span>6. スマートシティ</span></li>
+          <li><span>7. 有線から無線に変換</span></li>
+          <li><span>8. センサーネットワーク</span></li>
+          <li><span>9. カスタマイズ対応</span></li>
+          <li><span>10. 特殊用途対応</span></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <?php get_template_part( "template-parts/footer_top" ,"product"); ?>
+
+</main>
+<?php get_footer(); ?>
