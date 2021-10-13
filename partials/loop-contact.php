a776f103da72dae12503545dbfcfd5c00d3f6f7c
unknown <letrunghieu5612@gmail.com>
2021-10-13 15:23:19 +0700

    hoan thanh desktop

--- partials/loop-contact.php
+++ partials/loop-contact.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-<section class="banner-news">
+<section class="banner-contact">
     <div class="banner-wrapper">
-        <img src="assets/images/Banner-news.jpg" class="w-100" alt="">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/Banner-contact.jpg" class="w-100" alt="">
     </div>
 </section>
 <section class="contact-desc">
@@ -37,68 +37,8 @@
             <p>お問い合わせ内容を入力して、確認ボタンを押してください（確認画面へ進みます）</p>
         </div>
         <div class="form-place">
-            <form id="contactForm">
-                <div class="form-group">
-                    <label for="">件名</label>
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <label for="">会社名 </label>
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <div>
-                        <label for="">お名前</label>
-                        <span class="required-tag">必須</span>
-                    </div>
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <div> <label for="">ふりがな</label>
-                        <span class="required-tag">必須</span>
-                    </div>
-
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <div>
-                        <label for="">ご住所</label>
-                        <span class="required-tag">必須</span>
-                    </div>
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <div>
-                        <label for="">お電話番号</label>
-                        <span class="required-tag">必須</span>
-                    </div>
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <div>
-                        <label for="">メールアドレス</label>
-                        <span class="required-tag">必須</span>
-                    </div>
-                    <input type="text">
-                </div>
-                <div class="form-group">
-                    <div>
-                        <label for="">お問い合わせ内容</label>
-                        <span class="required-tag">必須</span>
-                    </div>
-                    <textarea>
-                        </textarea>
-                </div>
-                <input type="submit" class="btn-cus d-block mx-auto" value="送信内容を確認する">
-            </form>
-            <div class="box-guide">
-                <div class="head">
-                    <p class="fw-bold text-white">メールが届かない方へ</p>
-                </div>
-                <div class="foot">
-                    <p>お客様が迷惑メールなどの設定で、メールが正しく届かない場合がございます。</p>
-                    <p>【@musenplus.com】からのメールを受信できるように設定お願い申し上げます。</p>
-                </div>
+            <div id="contactForm">
+                <?php echo do_shortcode('[contact-form-7 id="15" title="Form liên hệ 1"]');  ?>
             </div>
         </div>
         <ul class="recomend-cate d-flex justify-content-between">
