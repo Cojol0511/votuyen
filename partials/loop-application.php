33e2b980544b726f7a159e4524e3f8aa7de18b52
unknown <letrunghieu5612@gmail.com>
2021-10-13 15:55:34 +0700

    them link o trang chu

--- partials/loop-application.php
+++ partials/loop-application.php
@@ -1,108 +1,112 @@
 <?php get_header(); ?>
 <main id="page-application">
 
-  <section class="banner"></section>
+    <section class="banner"></section>
 
-  <section class="header">
-    <h1 class="text-title purple-co">製品情報</h1>
-    <h2 class="text-sub-title blue-co">高品質＋低コスト＋迅速での開発</h2>
-  </section>
+    <section class="header">
+        <h1 class="text-title purple-co">製品情報</h1>
+        <h2 class="text-sub-title blue-co">高品質＋低コスト＋迅速での開発</h2>
+    </section>
 
-  <section class="">
-    <div class="container">
-      <div class="row">
-        <div class="col-5">
-          <img class="product-img"
-            src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part1.jpg" alt="">
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <img class="product-img"
+                        src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part1.jpg"
+                        alt="">
+                </div>
+                <div class="col-7">
+                    <div class="text2Col">
+                        <h2 class="text-sub-title blue-co">特徴</h2>
+                        <ul class="text-list">
+                            <li><span>特定小電力送信機</span></li>
+                            <li><span>シャッターリモコン</span></li>
+                            <li><span>特定小電力受信機</span></li>
+                            <li><span>ゲートリモコン</span></li>
+                            <li><span>産業用リモコン</span></li>
+                            <li><span>ウィンドウリモコン</span></li>
+                            <li><span>ワイヤレスリモコン</span></li>
+                            <li><span>カーテンリモコン</span></li>
+                            <li><span>オン/オフ制御</span></li>
+                            <li><span>マンションゲートコントロール</span></li>
+                            <li><span>双方向制御</span></li>
+                            <li><span>駐車場ゲート</span></li>
+                            <li><span>ハンズフリー制御</span></li>
+                            <li><span>無線プッシュスイッチ</span></li>
+                            <li><span>高セキュリティドアロック</span></li>
+                            <li><span>リモートインジケーター</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-7">
-          <div class="text2Col">
-            <h2 class="text-sub-title blue-co">特徴</h2>
-            <ul class="text-list">
-              <li><span>特定小電力送信機</span></li>
-              <li><span>シャッターリモコン</span></li>
-              <li><span>特定小電力受信機</span></li>
-              <li><span>ゲートリモコン</span></li>
-              <li><span>産業用リモコン</span></li>
-              <li><span>ウィンドウリモコン</span></li>
-              <li><span>ワイヤレスリモコン</span></li>
-              <li><span>カーテンリモコン</span></li>
-              <li><span>オン/オフ制御</span></li>
-              <li><span>マンションゲートコントロール</span></li>
-              <li><span>双方向制御</span></li>
-              <li><span>駐車場ゲート</span></li>
-              <li><span>ハンズフリー制御</span></li>
-              <li><span>無線プッシュスイッチ</span></li>
-              <li><span>高セキュリティドアロック</span></li>
-              <li><span>リモートインジケーター</span></li>
-            </ul>
-          </div>
+    </section>
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <img class="product-img"
+                        src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part2.jpg"
+                        alt="">
+                </div>
+                <div class="col-7">
+                    <div class="text1Col">
+                        <h2 class="text-sub-title green-co">トランシーバー</h2>
+                        <ul class="text-list">
+                            <li><span>LoRaモジュール</span></li>
+                            <li><span>Sigfoxモジュール</span></li>
+                            <li><span>ワイヤレスモジュール</span></li>
+                            <li><span>有線から無線に変換</span></li>
+                            <li><span>産業用リモコン</span></li>
+                            <li><span>双方向のデータ転送</span></li>
+                            <li><span>超長距離制御</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
-  <section class="">
-    <div class="container">
-      <div class="row">
-        <div class="col-5">
-          <img class="product-img"
-            src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part2.jpg" alt="">
+    </section>
+    <section class="">
+        <div class="container">
+            <div class="row gx-4">
+                <div class="col-6">
+                    <img class="product-img"
+                        src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part3.jpg"
+                        alt="">
+                </div>
+                <div class="col-6">
+                    <img class="product-img"
+                        src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part4.jpg"
+                        alt="">
+                </div>
+                <div class="col-12">
+                    <div class="text2Col" style="padding-top:50px">
+                        <h2 class="text-sub-title red-co">ワイヤレスシステム</h2>
+                        <ul class="text-list">
+                            <li><span>緊急通報</span></li>
+                            <li><span>遠隔モニタリング</span></li>
+                            <li><span>呼び出し</span></li>
+                            <li><span>IoTシステム</span></li>
+                            <li><span>介護用</span></li>
+                            <li><span>スマートナーシングホーム</span></li>
+                            <li><span>老人ホーム</span></li>
+                            <li><span>スマートホーム</span></li>
+                            <li><span>屋内、屋外の監視</span></li>
+                            <li><span>スマート農業</span></li>
+                            <li><span>呼吸・拍動などのヘルスケア・モニタリング</span></li>
+                            <li><span>スマートファクトリー</span></li>
+                            <li><span>浴室内などのプライベート空間での見守り</span></li>
+                            <li><span>スマートシティ</span></li>
+                            <li><span>セキュリティ応用</span></li>
+                            <li><span>センサーネットワーク</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-7">
-          <div class="text1Col">
-            <h2 class="text-sub-title green-co">トランシーバー</h2>
-            <ul class="text-list">
-              <li><span>LoRaモジュール</span></li>
-              <li><span>Sigfoxモジュール</span></li>
-              <li><span>ワイヤレスモジュール</span></li>
-              <li><span>有線から無線に変換</span></li>
-              <li><span>産業用リモコン</span></li>
-              <li><span>双方向のデータ転送</span></li>
-              <li><span>超長距離制御</span></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="">
-    <div class="container">
-      <div class="row gx-4">
-        <div class="col-6">
-          <img class="product-img"
-            src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part3.jpg" alt="">
-        </div>
-        <div class="col-6">
-          <img class="product-img"
-            src="<?php bloginfo('template_url'); ?>/assets/images/page-application/application-part4.jpg" alt="">
-        </div>
-        <div class="col-12">
-          <div class="text2Col" style="padding-top:50px">
-            <h2 class="text-sub-title red-co">ワイヤレスシステム</h2>
-            <ul class="text-list">
-              <li><span>緊急通報</span></li>
-              <li><span>遠隔モニタリング</span></li>
-              <li><span>呼び出し</span></li>
-              <li><span>IoTシステム</span></li>
-              <li><span>介護用</span></li>
-              <li><span>スマートナーシングホーム</span></li>
-              <li><span>老人ホーム</span></li>
-              <li><span>スマートホーム</span></li>
-              <li><span>屋内、屋外の監視</span></li>
-              <li><span>スマート農業</span></li>
-              <li><span>呼吸・拍動などのヘルスケア・モニタリング</span></li>
-              <li><span>スマートファクトリー</span></li>
-              <li><span>浴室内などのプライベート空間での見守り</span></li>
-              <li><span>スマートシティ</span></li>
-              <li><span>セキュリティ応用</span></li>
-              <li><span>センサーネットワーク</span></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
 
-  </section>
+    </section>
 
 </main>
 <?php get_footer(); ?>
