1f33e747dd8c469c254ec0662526bf03bae11dff
Cojol0511 <phunghoangphuloc@gmail.com>
2021-10-15 10:50:06 +0700

    mobile transceiver

--- partials/loop-transceiver.php
+++ partials/loop-transceiver.php
@@ -2,9 +2,9 @@
 <main id="page-transceiver" class="page-detail-product">
   <section class="banner-product">
     <div class="banner-text">
-      <h1 class="text-title">製品情報</h1>
-      <h2 class="text-sub-title">お客様のニーズに応え</h2>
-      <h2 class="text-sub-title">トランシーバー</h2>
+      <h1 class="text-title yellow-co">製品情報</h1>
+      <h2 class="text-sub-title purple-co">お客様のニーズに応え</h2>
+      <h2 class="text-sub-title blue-co">トランシーバー</h2>
     </div>
   </section>
 
@@ -14,7 +14,7 @@
       <div class="container">
         <div class="row">
           <h1 class="text-title">1. 無線モジュールシリーズ:</h1>
-          <div class="col-3">
+          <div class="col-md-3">
             <div class="menu">
               <ul>
                 <li>
@@ -35,7 +35,7 @@
               </ul>
             </div>
           </div>
-          <div class="col-9 text-end">
+          <div class="col-md-9 text-center text-md-end">
             <img class="product-img"
               src="<?php bloginfo('template_url'); ?>/assets/images/page-product/transceiver-part1.jpg" alt="">
           </div>
@@ -60,124 +60,126 @@
     <div class="table-product">
       <div class="container">
         <h2 class="text-sub-title">仕様</h2>
-        <table>
-          <thead>
-            <tr>
-              <td>項目</td>
-              <td>EcoMod315xx</td>
-              <td>EcoMod426xx</td>
-              <td>EcoMod429xx</td>
-              <td>EcoMod920xx</td>
-              <td>EcoTx2.EcoMod2.4Gxx</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>規格</td>
-              <td>ARIB STD-T93</td>
-              <td>ARIB STD-T67</td>
-              <td>ARIB STD-T67</td>
-              <td>ARIB STD-T108</td>
-              <td>ARIB STD-T66</td>
-            </tr>
-            <tr>
-              <td>周波数</td>
-              <td>312.1MHz～315.0MHz</td>
-              <td>426.0250MHz～426.1375MHz</td>
-              <td>429.2500～429.7375MHz</td>
-              <td>920.6MHz～928.0MHz</td>
-              <td>2400MHz～2483.5MHz</td>
-            </tr>
-            <tr>
-              <td>チャネル数</td>
-              <td>設定可</td>
-              <td>間隔12.5kHz: 10CH<br>間隔 6.25kHz: 18CH</td>
-              <td>間隔 12.5kHz: 40CH<br>間隔 6.25kHz: 79CH</td>
-              <td>間隔 200kHz: 38CH</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>変調方式</td>
-              <td>GFSK</td>
-              <td>GFSK</td>
-              <td>GFSK</td>
-              <td>GFSK</td>
-              <td>スペクトラム拡散</td>
-            </tr>
-            <tr>
-              <td>時間制限</td>
-              <td>90s on, 一瞬 off</td>
-              <td>5s on, 2s off </td>
-              <td>連続可</td>
-              <td>4s on, 0.05s off </td>
-              <td>連続可</td>
-            </tr>
-            <tr>
-              <td>実際ビットレート</td>
-              <td>設定可</td>
-              <td>2.4kbps/4.8kbps</td>
-              <td>2.4kbps/4.8kbps</td>
-              <td>2.4kbps～100kbps</td>
-              <td>250kbps</td>
-            </tr>
-            <tr>
-              <td>送信パワー</td>
-              <td>0.25mW (EIRP)</td>
-              <td>10mW</td>
-              <td>10mW</td>
-              <td>20mW</td>
-              <td>200mW</td>
-            </tr>
-            <tr>
-              <td>温度補償共振器</td>
-              <td>TCXO</td>
-              <td>TCXO</td>
-              <td>TCXO</td>
-              <td>TCXO</td>
-              <td>TCXO</td>
-            </tr>
-            <tr>
-              <td>インタフェースタイプ</td>
-              <td>指定可</td>
-              <td>指定可</td>
-              <td>指定可</td>
-              <td>指定可</td>
-              <td>指定可</td>
-            </tr>
-            <tr>
-              <td>アンテナ</td>
-              <td>内蔵／外部</td>
-              <td>内蔵／外部</td>
-              <td>内蔵／外部</td>
-              <td>内蔵／外部</td>
-              <td>内蔵／外部</td>
-            </tr>
-            <tr>
-              <td>電源</td>
-              <td>指定可</td>
-              <td>指定可</td>
-              <td>指定可</td>
-              <td>指定可</td>
-              <td>指定可</td>
-            </tr>
-            <tr>
-              <td>消費電流</td>
-              <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 15mA</td>
-              <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 29mA</td>
-              <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 19mA</td>
-              <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 29mA</td>
-              <td>スリープ 0.1mA<br>受信時 15mA<br>送信時 150mA</td>
-            </tr>
-            <tr>
-              <td>見通し通信距離</td>
-              <td>約300m</td>
-              <td>約1000m</td>
-              <td>約1000m</td>
-              <td>約2000m</td>
-              <td>約1000m</td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="table-responsive">
+          <table>
+            <thead>
+              <tr>
+                <td>項目</td>
+                <td>EcoMod315xx</td>
+                <td>EcoMod426xx</td>
+                <td>EcoMod429xx</td>
+                <td>EcoMod920xx</td>
+                <td>EcoTx2.EcoMod2.4Gxx</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>規格</td>
+                <td>ARIB STD-T93</td>
+                <td>ARIB STD-T67</td>
+                <td>ARIB STD-T67</td>
+                <td>ARIB STD-T108</td>
+                <td>ARIB STD-T66</td>
+              </tr>
+              <tr>
+                <td>周波数</td>
+                <td>312.1MHz～315.0MHz</td>
+                <td>426.0250MHz～426.1375MHz</td>
+                <td>429.2500～429.7375MHz</td>
+                <td>920.6MHz～928.0MHz</td>
+                <td>2400MHz～2483.5MHz</td>
+              </tr>
+              <tr>
+                <td>チャネル数</td>
+                <td>設定可</td>
+                <td>間隔12.5kHz: 10CH<br>間隔 6.25kHz: 18CH</td>
+                <td>間隔 12.5kHz: 40CH<br>間隔 6.25kHz: 79CH</td>
+                <td>間隔 200kHz: 38CH</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>変調方式</td>
+                <td>GFSK</td>
+                <td>GFSK</td>
+                <td>GFSK</td>
+                <td>GFSK</td>
+                <td>スペクトラム拡散</td>
+              </tr>
+              <tr>
+                <td>時間制限</td>
+                <td>90s on, 一瞬 off</td>
+                <td>5s on, 2s off </td>
+                <td>連続可</td>
+                <td>4s on, 0.05s off </td>
+                <td>連続可</td>
+              </tr>
+              <tr>
+                <td>実際ビットレート</td>
+                <td>設定可</td>
+                <td>2.4kbps/4.8kbps</td>
+                <td>2.4kbps/4.8kbps</td>
+                <td>2.4kbps～100kbps</td>
+                <td>250kbps</td>
+              </tr>
+              <tr>
+                <td>送信パワー</td>
+                <td>0.25mW (EIRP)</td>
+                <td>10mW</td>
+                <td>10mW</td>
+                <td>20mW</td>
+                <td>200mW</td>
+              </tr>
+              <tr>
+                <td>温度補償共振器</td>
+                <td>TCXO</td>
+                <td>TCXO</td>
+                <td>TCXO</td>
+                <td>TCXO</td>
+                <td>TCXO</td>
+              </tr>
+              <tr>
+                <td>インタフェースタイプ</td>
+                <td>指定可</td>
+                <td>指定可</td>
+                <td>指定可</td>
+                <td>指定可</td>
+                <td>指定可</td>
+              </tr>
+              <tr>
+                <td>アンテナ</td>
+                <td>内蔵／外部</td>
+                <td>内蔵／外部</td>
+                <td>内蔵／外部</td>
+                <td>内蔵／外部</td>
+                <td>内蔵／外部</td>
+              </tr>
+              <tr>
+                <td>電源</td>
+                <td>指定可</td>
+                <td>指定可</td>
+                <td>指定可</td>
+                <td>指定可</td>
+                <td>指定可</td>
+              </tr>
+              <tr>
+                <td>消費電流</td>
+                <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 15mA</td>
+                <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 29mA</td>
+                <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 19mA</td>
+                <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 29mA</td>
+                <td>スリープ 0.1mA<br>受信時 15mA<br>送信時 150mA</td>
+              </tr>
+              <tr>
+                <td>見通し通信距離</td>
+                <td>約300m</td>
+                <td>約1000m</td>
+                <td>約1000m</td>
+                <td>約2000m</td>
+                <td>約1000m</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </section>
@@ -188,10 +190,10 @@
       <div class="container">
         <div class="row">
           <h1 class="text-title">2. LoRaモジュールシリーズ:</h1>
-          <div class="col-3">
+          <div class="col-md-3">
             <div class="menu">
               <ul>
-                <li>
+                <li class="me-2">
                   <h2 class="text-menu">SLR429A1</h2>
                 </li>
                 <li>
@@ -200,7 +202,7 @@
               </ul>
             </div>
           </div>
-          <div class="col-9 text-end">
+          <div class="col-md-9 text-center text-md-end">
             <img class="product-img"
               src="<?php bloginfo('template_url'); ?>/assets/images/page-product/transceiver-part2.jpg" alt="">
           </div>
@@ -225,102 +227,104 @@
     <div class="table-product">
       <div class="container">
         <h2 class="text-sub-title">仕様</h2>
-        <table>
-          <thead>
-            <tr>
-              <td>項目</td>
-              <td>SLR429xx</td>
-              <td>SLR920xx</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>規格</td>
-              <td>ARIB STD-T67</td>
-              <td>ARIB STD-T108</td>
-            </tr>
-            <tr>
-              <td>周波数</td>
-              <td>429.2500～429.7375MHz</td>
-              <td>920.6MHz～928.0MHz</td>
-            </tr>
-            <tr>
-              <td>チャネル数</td>
-              <td>間隔 12.5kHz: 40CH<br>間隔 6.25kHz: 79CH</td>
-              <td>間隔 200kHz: 38CH</td>
-            </tr>
-            <tr>
-              <td>変調方式</td>
-              <td>スペクトラム拡散</td>
-              <td>スペクトラム拡散</td>
-            </tr>
-            <tr>
-              <td>時間制限</td>
-              <td>連続可</td>
-              <td>4s on, 0.05s off</td>
-            </tr>
-            <tr>
-              <td>実際ビットレート</td>
-              <td>0.013kbps～0.2kbps</td>
-              <td>0.013kbps～2.0kbps</td>
-            </tr>
-            <tr>
-              <td>送信パワー</td>
-              <td>10mW</td>
-              <td>20mW</td>
-            </tr>
-            <tr>
-              <td>温度補償共振器</td>
-              <td>TCXO</td>
-              <td>TCXO</td>
-            </tr>
-            <tr>
-              <td>インタフェースタイプ</td>
-              <td>指定可</td>
-              <td>指定可</td>
-            </tr>
-            <tr>
-              <td>アンテナ</td>
-              <td>内蔵／外部</td>
-              <td>内蔵／外部</td>
-            </tr>
-            <tr>
-              <td>電源</td>
-              <td>指定可</td>
-              <td>指定可</td>
-            </tr>
-            <tr>
-              <td>消費電流</td>
-              <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 19mA</td>
-              <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 29mA</td>
-            </tr>
-            <tr>
-              <td>見通し通信距離</td>
-              <td>約15km</td>
-              <td>約30km</td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="table-responsive">
+          <table>
+            <thead>
+              <tr>
+              <tr>
+                <td>項目</td>
+                <td>SLR429xx</td>
+                <td>SLR920xx</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>規格</td>
+                <td>ARIB STD-T67</td>
+                <td>ARIB STD-T108</td>
+              </tr>
+              <tr>
+                <td>周波数</td>
+                <td>429.2500～429.7375MHz</td>
+                <td>920.6MHz～928.0MHz</td>
+              </tr>
+              <tr>
+                <td>チャネル数</td>
+                <td>間隔 12.5kHz: 40CH<br>間隔 6.25kHz: 79CH</td>
+                <td>間隔 200kHz: 38CH</td>
+              </tr>
+              <tr>
+                <td>変調方式</td>
+                <td>スペクトラム拡散</td>
+                <td>スペクトラム拡散</td>
+              </tr>
+              <tr>
+                <td>時間制限</td>
+                <td>連続可</td>
+                <td>4s on, 0.05s off</td>
+              </tr>
+              <tr>
+                <td>実際ビットレート</td>
+                <td>0.013kbps～0.2kbps</td>
+                <td>0.013kbps～2.0kbps</td>
+              </tr>
+              <tr>
+                <td>送信パワー</td>
+                <td>10mW</td>
+                <td>20mW</td>
+              </tr>
+              <tr>
+                <td>温度補償共振器</td>
+                <td>TCXO</td>
+                <td>TCXO</td>
+              </tr>
+              <tr>
+                <td>インタフェースタイプ</td>
+                <td>指定可</td>
+                <td>指定可</td>
+              </tr>
+              <tr>
+                <td>アンテナ</td>
+                <td>内蔵／外部</td>
+                <td>内蔵／外部</td>
+              </tr>
+              <tr>
+                <td>電源</td>
+                <td>指定可</td>
+                <td>指定可</td>
+              </tr>
+              <tr>
+                <td>消費電流</td>
+                <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 19mA</td>
+                <td>スリープ 0.1mA<br>受信時 9mA<br>送信時 29mA</td>
+              </tr>
+              <tr>
+                <td>見通し通信距離</td>
+                <td>約15km</td>
+                <td>約30km</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-    <div class="text">
-      <div class="container">
-        <h2 class="text-sub-title">用途／対応製品</h2>
-        <ul class="feature-list">
-          <li><span>1. 特定小電力トランシーバー</span></li>
-          <li><span>2. LoRaモジュール</span></li>
-          <li><span>3. Sigfoxモジュール</span></li>
-          <li><span>4. 無線モジュール</span></li>
-          <li><span>5. リモートモニタリングシステム</span></li>
-          <li><span>6. センサーネットワーク</span></li>
-          <li><span>7. 遠隔監視システム</span></li>
-          <li><span>8. 遠隔制御システム</span></li>
-          <li><span>9. 有線から無線に変換</span></li>
-          <li><span>10. カスタマイズ対応</span></li>
-          <li><span>11. 特殊用途対応</span></li>
-        </ul>
+      <div class="text">
+        <div class="container">
+          <h2 class="text-sub-title">用途／対応製品</h2>
+          <ul class="feature-list">
+            <li><span>1. 特定小電力トランシーバー</span></li>
+            <li><span>2. LoRaモジュール</span></li>
+            <li><span>3. Sigfoxモジュール</span></li>
+            <li><span>4. 無線モジュール</span></li>
+            <li><span>5. リモートモニタリングシステム</span></li>
+            <li><span>6. センサーネットワーク</span></li>
+            <li><span>7. 遠隔監視システム</span></li>
+            <li><span>8. 遠隔制御システム</span></li>
+            <li><span>9. 有線から無線に変換</span></li>
+            <li><span>10. カスタマイズ対応</span></li>
+            <li><span>11. 特殊用途対応</span></li>
+          </ul>
+        </div>
       </div>
-    </div>
   </section>
 
 
