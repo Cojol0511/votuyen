5e5139d0481d90b46e24888db2452a4df9b88f9a
Cojol0511 <phunghoangphuloc@gmail.com>
2021-10-16 08:35:09 +0700

    fix break point respone and change title modal

--- partials/loop-product-list.php
+++ partials/loop-product-list.php
@@ -138,7 +138,7 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-fullscreen-md-down">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="ModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="ModalLabel">用途／対応製品</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
